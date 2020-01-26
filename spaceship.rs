b32c5cb0b5d54318436895c63cccd880a2818168
alpheonix <arthurblanc98@gmail.com>
2020-01-26 21:33:55 +0100

    debut

---
+++ spaceship.rs
@@ -0,0 +1,19 @@
+pub struct Spaceship {
+    pub _id: String,
+    pub name: String,
+    pub speed: i64,
+    pub hp: i64,
+    pub damage: i64,
+    pub adventage: bool
+}
+
+pub fn create_spaceship_template() -> Spaceship {
+    Spaceship{
+    _id: "1".to_string(),
+    name: "X-wing".to_string(),
+    speed: 100,
+    hp: 92000,
+    damage: 24000,
+    adventage: false
+    }
+}
